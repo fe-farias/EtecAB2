da964fa23d2ab6d8d220c5002fd878de423eefdb
Felipe Farias <fefarias97@gmail.com>
2021-08-24 13:07:19 -0300

    Resolução do exercício.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EtecAB2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Etec Adolpho Berezin");
+        }
+    }
+}
